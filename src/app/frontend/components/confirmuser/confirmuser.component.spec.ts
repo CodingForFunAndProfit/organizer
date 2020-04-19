72aa4319dd3c89b52353fcf38c399c54a8fe2dda
CodingForFunAndProfit <mail@florianstolz.net>
2020-04-20 00:12:15 +0200

    fixed the test

--- src/app/frontend/components/confirmuser/confirmuser.component.spec.ts
+++ src/app/frontend/components/confirmuser/confirmuser.component.spec.ts
@@ -1,25 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfirmuserComponent } from './confirmuser.component';
+import { AuthService } from 'src/app/services/auth.service';
+import { ApolloTestingModule } from 'apollo-angular/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('ConfirmuserComponent', () => {
-  let component: ConfirmuserComponent;
-  let fixture: ComponentFixture<ConfirmuserComponent>;
+    let component: ConfirmuserComponent;
+    let fixture: ComponentFixture<ConfirmuserComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ConfirmuserComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ConfirmuserComponent],
+            imports: [
+                ApolloTestingModule,
+                RouterTestingModule,
+                BrowserAnimationsModule,
+                MaterialModule,
+                HttpClientTestingModule,
+            ],
+            providers: [AuthService],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ConfirmuserComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ConfirmuserComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
